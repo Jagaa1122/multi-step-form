01c543de9cd6dd6fc4ead6ac6bb7118a8f9e2505
Jagaa1122 <chjagaa@icloud.com>
2025-01-13 11:33:28 +0800

    first commit

---
+++ src/components/Step1.jsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center ">
+      <div className="bg-[#ffffff] w-[480px] h-[655px] p-[32px] justify-between">
+        <img src="Main 1.png" alt="" />
+        <h1 className="text-[#202124] font-bold text-[26px]">Join Us! 😎</h1>
+        <p className="text-[#8E8E8E]">
+          Please provide all current information accurately.
+        </p>
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+}
