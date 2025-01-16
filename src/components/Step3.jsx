c19241e12b693b4bc2bd9878fbe09c5d622fd3fd
Jagaa1122 <chjagaa@icloud.com>
2025-01-16 12:47:03 +0800

    profile image dutuu

--- src/components/Step3.jsx
+++ src/components/Step3.jsx
@@ -2,6 +2,16 @@ import Input from "@/components/Input";
 import Buttons from "@/components/Buttons";
 import { useState } from "react";
 export default function Home({ currentStep, onClick, goBack }) {
+  const [imageUrl, setImageUrl] = useState();
+  // const onFileUpload = event.target.files[0];
+  // setImageUrl(URL.createObjectURL(file));
+  const onFileUpload = (event) => {
+    const file = event.target.files[0];
+
+    // const url = ;
+    setImageUrl(URL.createObjectURL(file));
+  };
+
   const [formValues, setFormValues] = useState({
     dateOfBirth: "",
     profileImage: "",
@@ -26,20 +36,21 @@ export default function Home({ currentStep, onClick, goBack }) {
       newErrors.dateOfBirth = "Please select a date.";
       isValid = false;
     } else {
-    const currentDate = new Date();
-    const birthDate = new Date(formValues.dateOfBirth);
-    const age = currentDate.getFullYear() - birthDate.getFullYear();
-    const monthDifference = currentDate.getMonth() - birthDate.getMonth();
-    const dayDifference = currentDate.getDate() - birthDate.getDate();
+      const currentDate = new Date();
+      const birthDate = new Date(formValues.dateOfBirth);
+      const age = currentDate.getFullYear() - birthDate.getFullYear();
+      const monthDifference = currentDate.getMonth() - birthDate.getMonth();
+      const dayDifference = currentDate.getDate() - birthDate.getDate();
 
-    if (
-      age < 18 || 
-      (age === 18 && (monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)))
-    ) {
-      newErrors.dateOfBirth = "You must be at least 18 years old.";
-      isValid = false;
+      if (
+        age < 18 ||
+        (age === 18 &&
+          (monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)))
+      ) {
+        newErrors.dateOfBirth = "You must be at least 18 years old.";
+        isValid = false;
+      }
     }
-  }
 
     if (!formValues.profileImage.trim()) {
       newErrors.profileImage = "Image cannot be blank";
@@ -72,13 +83,34 @@ export default function Home({ currentStep, onClick, goBack }) {
           name="dateOfBirth"
           type="date"
         />
-        <Input 
+        {/* <Input
           label="Profile image"
-            errortext={formErrors.profileImage}
-          onChange={handleChange}
+          errortext={formErrors.profileImage}
+          onChange={onFileUpload}
           name="profileImage"
           type="file"
-        />
+        /> */}
+        <div id="profileImage">
+          <h2>
+            Profile image <span className="text-[#e14942]">*</span>
+          </h2>
+          <div className="bg-zinc-200 w-full h-[200px] flex justify-center items-center">
+            <label
+              htmlFor="file-input"
+              className="bg-zinc-200 rounded-lg cursor-pointer"
+            >
+              <input
+                hidden
+                id="file-input"
+                type="file"
+                onChange={onFileUpload}
+              ></input>
+              <img className="object-cover" src={imageUrl} alt="Upload image" />
+              {/* <h1>Upload image</h1> */}
+            </label>
+          </div>
+          <p>{formErrors.profileImage}</p>
+        </div>
       </div>
       <div className="flex mt-auto text-center pt-8 gap-2 ">
         <button
